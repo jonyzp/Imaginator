e73cd730c2659c8a154d60472a8fdcd1670be104
MauricioHoyosArdila <mhoyosa2@eafit.edu.co>
2017-09-18 09:34:59 -0500

    conexión a la DB

--- config/config.js
+++ config/config.js
@@ -21,7 +21,7 @@ var config = {
       name: 'imaginator'
     },
     port: process.env.PORT || 8084,
-    db: 'mongodb://localhost/imaginator'
+    db: 'mongodb://server0,server1,server2,server3/?replicaSet=myreplica01'
   },
 
   production: {
